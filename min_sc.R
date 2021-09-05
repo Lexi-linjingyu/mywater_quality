3fa47588f1d9f26f71fdc55ae1d23fb806549c1a
Lexi-linjingyu <linjingy@deakin.edu.au>
2021-09-05 20:59:42 +1000

    update

--- min_sc.R
+++ min_sc.R
@@ -2,32 +2,38 @@ Packages <- c("SWATplusR","purrr","sf","tidyr","fast","dplyr","ggplot2", "sensit
               "lhs","hydromad","lubridate","hydroGOF","forcats")
 lapply(Packages, library, character.only = TRUE)
 
-path <- "D:/lexi/min_sc_TxtInOut"
+path <- "E:/Lexi/data_paper3/min_sc_TxtInOut"
 
 # Path to the subbasin shape file
-sub_path <- "D:/lexi/min_sc_TxtInOut/Shapes/subs1.shp"
-# Path to the subbasin shape file
-riv_path <- "D:/lexi/min_sc_TxtInOut/Shapes/riv1.shp"
-#> [1] "/home/christoph/R/x86_64-pc-linux-gnu-library/3.6/SWATdata/extdata/plus_shapes/sub.shp"
+sub_path <- "E:/Lexi/data_paper3/min_sc_TxtInOut/Shapes/subs1.shp"
+riv_path <- "E:/Lexi/data_paper3/min_sc_TxtInOut/Shapes/riv1.shp"
+# Path to run model 
+run_path_qu = "E:/Lexi/data_paper3/mywater/water_quality"
+run_path_nh4 = "E:/Lexi/data_paper3/mywater/water_quantity"
+
+# Read shapefile from the path
 sub <- read_sf(sub_path)
 riv <- read_sf(riv_path)
 
-## step 1 model simulation##
-q_sim_min_1960 <- run_swat2012(project_path = path,
+# Read observed data and change format
+q_obs_min <- read.csv("E:/Lexi/data_paper3/min_sc_TxtInOut/min_river.csv")
+nh4_obs_min <- read.csv()
+q_obs_min$date <- as.Date(as.character(qu_obs_min$date),format ="%Y-%m-%d")
+
+#### Step 1 model simulation ####
+sim_min_1960 <- run_swat2012(project_path = path,
                                start_date = "1981-01-01",
                                end_date = "2019-12-31",
-                               output = list(q_out = define_output(file = "rch",
+                               output = list(nh4_out = define_output(file = "rch",
+                                                                variable = "NH4_OUT",
+                                                                unit = 1:94),
+                                             q_out = define_output(file = "rch",
                                                                    variable = "FLOW_OUT",
-                                                                   unit = 1:94),
-                                             q_tn = define_output(file = "rch",
-                                                                  variable = "TOT_N",
-                                                                  unit = 1:94),
-                                             q_nh4 =define_output(file = "rch",
-                                                                  variable = "NH4_OUT",
-                                                                  unit = 1:94)))
-plot(q_sim_min_1960$date,q_sim_min_1960$q_tn_29,type="l",lty=1,ylim=c(0,10000000))
-q_obs_min <- read.csv("E:/Lexi/data_paper3/min_sc_TxtInOut/min_river.csv")
-q_obs_min$date <- as.Date(as.character(q_obs_min$date),format ="%Y-%m-%d")
+                                                                   unit = 1:94)),
+                               run_path = run_path_qu)
+
+
+
 plot(q_obs_min,ylim = c(0,2000))
 plot(q_sim_min_1960$FLOW_OUT_29,ylim = c(0,1000))
 ggplot() +
@@ -64,45 +70,49 @@ plot(q_obs_maidilong$discharge,q_sim_2012$FLOW_OUT_10)
 r_sim <- lm(q_obs_maidilong$discharge ~ q_sim_2012$FLOW_OUT_10)
 summary(r_sim)
 
-## STEP 2 Sensitivity analysis##
-par_names <- c("CN2.mgt | change = relchg",
-               "GWQMN.gw | change = absval",
-               "ALPHA_BF.gw | change = absval",
-               "GW_DELAY.gw | change = absval",
-               "GW_REVAP.gw | change = relchg",
-               "ESCO.hru | change = absval",
-               "ALPHA_BNK.rte | change = absval",
-               "CH_N2.rte | change = absval",
-               "CH_K2.rte | change = absval",
-               "SOL_AWC.sol | change = relchg",
-               "SOL_K.sol | change = relchg",
-               "SOL_BD.sol | change = relchg",
-               "SMTMP.bsn | change = absval",
-               "SMFMX.bsn | change = absval")
-## "CDN.bsn | change = absval",(0:3)
-##"RCN.bsn | change = absval",(0,10)
-##"SOL_NO3.chm | change = relchg")  (0,300)          
-##  "FER_SURFACE.mgt | change = relchg"(0:1),
-## 
-par_fast <- fast_parameters(
-  minimum = c(0, 0, 0, 0, 0.02,0.01, 0, 0, 0, -0.5,-0.5, 1.1, -5, 1),
-  maximum = c(100, 5000, 1, 1, 0.2, 1, 1, 0.4, 200, 0.5, 0.5, 1.9, 5, 8),
-  names = par_names)
+#### Step 2 Sensitivity analysis ####
+par_names_all <- c("CDN.bsn | change = absval",
+                  "RCN.bsn | change = absval",
+                  "SOL_NO3.chm | change = absval",
+                  "NPERCO.bsn| change = absval",
+                  "ERORGH.hru| change = absval",
+                  "RS3.chm | change = absval",
+                  "SOL_ORGN.chm | change = absval",
+                  "BC1.chm | change = absval",
+                  "BC3.chm | change = absval",
+                  "RSDCO.bsn | change = absval",
+                  "CN2.mgt | change = relchg",
+                  "SOL_AWC.sol | change = relchg",
+                  "SOL_K.sol | change = relchg",
+                  "ALPHA_BF.gw | change = absval",
+                  "ALPHA_BNK.rte | change = absval",
+                  "GWQMN.gw | change = absval",
+                  "CH_N2.rte | change = absval",
+                  "CH_K2.rte | change = absval"
+                  )
+par_fast_all <- fast_parameters(
+  minimum = c(0, 0, 0, 0, 0, 0.001, 0, 0.1, 0.2, 0, 0, 0,-0.5, 0, 0, 0, 0, 100),
+  maximum = c(3, 10, 300, 1, 5, 0.1, 100, 1, 0.4, 1, 100, 1, 0.5, 1, 0.5, 10, 0.4, 200),
+  names = par_names_all)
 
-q_fast <- run_swat2012(project_path = path,
-                       output  = list(q_out = define_output(file = "rch",
-                                                            variable = "FLOW_OUT",
-                                                            unit = 29)),
+q_fast_all <- run_swat2012(project_path = path,
+                       list(q_out = define_output(file = "rch",
+                                                  variable = "FLOW_OUT",
+                                                  unit = c(74,92)),
+                            q_nh4 = define_output(file = "rch",
+                                                 variable = "NH4_OUT",
+                                                 unit = c(74,92))),
                        
-                       parameter = par_fast,
+                       parameter = par_fast_all,
                        start_date = "1983-01-01",
                        end_date = "2008-12-31",
+                       run_path = run_path_nh4,
                        years_skip = 2,
-                       n_thread = 32)
+                       n_thread = 12)
 
-q_obs <- filter(q_obs_min, date >= ymd("2002-01-01"),
+q_obs <- filter(qu_obs_min, date >= ymd("2002-01-01"),
                 date <= "2008-12-31")
-nse_fast <- q_fast$simulation$q_out %>%
+nse_fast <- q_fast_qaqu$simulation$q_out %>%
   select(-date) %>%
   map_dbl(., ~NSE(.x, q_obs$discharge))
 sens_fast <- sensitivity(nse_fast, 12)
@@ -127,43 +137,42 @@ ggplot(data = dotty) +
   theme_bw()
 
 
-## Step 3 calibrate ##======================================================
-par_test_d <- tibble("CN2.mgt | change = relchg | sub = 1:94" = c(0,100),
-                     "SOL_AWC.sol | change = relchg | sub = 1:94" = c(0,1),
-                     "SOL_K.sol | change = relchg | sub = 1:94" = c(-0.5,0.5),
-                     "ALPHA_BF.gw | change = absval | sub = 1:94" = c(0,1),
-                     "ALPHA_BNK.rte | change = absval | sub = 1:94" = c(0,1),
-                     "GWQMN.gw | change = absval | sub = 1:10" = c(0,3000),
-                     "CH_N2.rte | change = absval | sub = 1:94" = c(0,0.4),
-                     "CH_K2.rte | change = absval | sub = 1:94" = c(0,200)
-)
+#### Step 3 calibrate ####======================================================
+par_test_nh4 <- tibble("CDN.bsn | change = absval | sub = 1:94" = c(0,3),
+                       "RCN.bsn | change = absval | sub = 1:94" = c(0,10),
+                       "SOL_NO3.chm | change = relchg | sub = 1:94" = c(0,300),
+                       "CN2.mgt | change = relchg | sub = 1:94" = 73.3,
+                       "SOL_AWC.sol | change = relchg | sub = 1:94" = 0.59,
+                       "SOL_K.sol | change = relchg | sub = 1:94"= -0.38,
+                       "ALPHA_BF.gw | change = absval | sub = 1:94" = 0.40,
+                       "ALPHA_BNK.rte | change = absval | sub = 1:94" = 0.1,
+                       "GWQMN.gw | change = absval | sub = 1:94" = 435,
+                       "CH_N2.rte | change = absval | sub = 1:94" = 0.21,
+                       "CH_K2.rte | change = absval | sub = 1:94" = 136)
 
 
-
-#"GWQMN.gw | change = absval | sub = 1:10" = runif(4,100,300)
-#"GW_DELAY.gw | change = absval | sub = 1:10" = runif(4,0,1000),
-# "GW_REVAP.gw | change = absval | sub = 1:10" = runif(4,1,2)
-# SMTMP.bsn | change = absval | sub = 1:10" = c(-20,-15),
-# SMFMX.bsn | change = absval | sub = 1:10" = c(0,0.1))
-n_sample = 400
-n_par = ncol(par_test_d)
+n_sample = 100
+n_par = ncol(par_test_nh4)
 par_iter1 <- randomLHS(n = n_sample, k = n_par) %>%
   as_tibble(., .name_repair = "minimal") %>%
-  set_names(names(par_test_d))%>%
-  map2_df(., par_test_d, ~ (.x * (.y[2] - .y[1]) + .y[1])) 
-q_test_d <- run_swat2012(project_path = path,
-                         output  = list(q_out = define_output(file = "rch",
-                                                              variable = "FLOW_OUT",
-                                                              unit = 29)),
+  set_names(names(par_test_nh4))%>%
+  map2_df(., par_test_nh4, ~ (.x * (.y[2] - .y[1]) + .y[1])) 
+nh4_test_d <- run_swat2012(project_path = path,
+                         list(nh4_out = define_output(file = "rch",
+                                                    variable = "NH4_OUT",
+                                                    unit = c(74,92),
+                              q_out = define_output(file = "rch",
+                                                    variable = "FLOW_OUT",
+                                                    unit = c(74,92))),
                          output_interval = "d",
                          parameter = par_iter1,
-                         start_date = "1983-01-01",
-                         end_date = "2008-12-31",
+                         start_date = "2005-01-01",
+                         end_date = "2018-12-31",
                          years_skip = 2,
-                         n_thread = 64)
+                         n_thread = 8))
 
 q_obs_min_test <- filter(q_obs_min, date >= "1985-01-01",date <= "2008-12-31")
-##q_test <- filter(q_test$simulation$q_out, date >= ymd("2004-03-20"),date <= "2012-12-31")
+
 
 nse_test <- q_test_d$simulation$q_out %>%
   select(-date) %>%
